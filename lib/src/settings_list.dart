be679fbddf2674928e317550be4cc09a0975c04e
Kajetan Puchalski <kajetan.puchalski@tuta.io>
2020-09-06 15:05:47 +0100

    Fixed InkWell splash not showing up on Android
    Replaced Container with Ink inside SettingsList
    Wrapped SettingsList in Material
    Solves issue #27

--- lib/src/settings_list.dart
+++ lib/src/settings_list.dart
@@ -23,30 +23,32 @@ class SettingsList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Theme.of(context).brightness == Brightness.light
-          ? backgroundColor ?? lightBackgroundColor ?? backgroundGray
-          : backgroundColor ?? darkBackgroundColor ?? Colors.black,
-      child: ListView.builder(
-        physics: physics,
-        shrinkWrap: shrinkWrap,
-        itemCount: sections.length,
-        itemBuilder: (context, index) {
-          AbstractSection current = sections[index];
-          AbstractSection futureOne;
-          try {
-            futureOne = sections[index + 1];
-          } catch (e) {}
+    return Material(
+      child: Ink(
+        color: Theme.of(context).brightness == Brightness.light
+            ? backgroundColor ?? lightBackgroundColor ?? backgroundGray
+            : backgroundColor ?? darkBackgroundColor ?? Colors.black,
+        child: ListView.builder(
+          physics: physics,
+          shrinkWrap: shrinkWrap,
+          itemCount: sections.length,
+          itemBuilder: (context, index) {
+            AbstractSection current = sections[index];
+            AbstractSection futureOne;
+            try {
+              futureOne = sections[index + 1];
+            } catch (e) {}
 
-          // Add divider if title is null
-          if (futureOne != null && futureOne.title != null) {
-            current.showBottomDivider = false;
-            return current;
-          } else {
-            current.showBottomDivider = true;
-            return current;
-          }
-        },
+            // Add divider if title is null
+            if (futureOne != null && futureOne.title != null) {
+              current.showBottomDivider = false;
+              return current;
+            } else {
+              current.showBottomDivider = true;
+              return current;
+            }
+          },
+        ),
       ),
     );
   }
