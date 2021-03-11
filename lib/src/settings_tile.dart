88b17effcf9b168835c10bb9a8ad2215e08f455f
Julian Steenbakker <juliansteenbakker@outlook.com>
2021-03-11 15:33:14 +0100

    Add slider tile

--- lib/src/settings_tile.dart
+++ lib/src/settings_tile.dart
@@ -1,12 +1,13 @@
 import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:settings_ui/src/cupertino_settings_item.dart';
 
 import 'defines.dart';
 
-enum _SettingsTileType { simple, switchTile }
+enum _SettingsTileType { simple, switchTile, sliderTile }
 
 class SettingsTile extends StatelessWidget {
   final String title;
@@ -27,6 +28,17 @@ class SettingsTile extends StatelessWidget {
   final Color switchActiveColor;
   final _SettingsTileType _tileType;
 
+  // Values for Slider
+  final double value;
+  final ValueChanged<double> onChanged;
+  final ValueChanged<double> onChangeStart;
+  final ValueChanged<double> onChangeEnd;
+  final double min;
+  final double max;
+  final int divisions;
+  final Color activeColor;
+  final Color thumbColor;
+
   const SettingsTile({
     Key key,
     @required this.title,
@@ -42,7 +54,7 @@ class SettingsTile extends StatelessWidget {
     this.subtitleTextStyle,
     this.enabled = true,
     this.onPressed,
-    this.switchActiveColor,
+    this.switchActiveColor, this.value, this.onChanged, this.onChangeStart, this.onChangeEnd, this.min, this.max, this.divisions, this.activeColor, this.thumbColor,
   })  : _tileType = _SettingsTileType.simple,
         onToggle = null,
         switchValue = null,
@@ -63,7 +75,7 @@ class SettingsTile extends StatelessWidget {
     @required this.switchValue,
     this.titleTextStyle,
     this.subtitleTextStyle,
-    this.switchActiveColor,
+    this.switchActiveColor, this.value, this.onChanged, this.onChangeStart, this.onChangeEnd, this.min, this.max, this.divisions, this.activeColor, this.thumbColor,
   })  : _tileType = _SettingsTileType.switchTile,
         onTap = null,
         onPressed = null,
@@ -71,6 +83,38 @@ class SettingsTile extends StatelessWidget {
         iosChevronPadding = null,
         assert(titleMaxLines == null || titleMaxLines > 0),
         assert(subtitleMaxLines == null || subtitleMaxLines > 0),
+        super(key: key);
+
+  const SettingsTile.sliderTile({
+    Key key,
+    this.titleMaxLines,
+    this.leading,
+    this.enabled = true,
+    this.trailing,
+    @required this.value,
+    @required this.onChanged,
+    this.onChangeStart,
+    this.onChangeEnd,
+    this.min = 0.0,
+    this.max = 1.0,
+    this.divisions,
+    this.activeColor,
+    this.thumbColor = CupertinoColors.white,
+    this.titleTextStyle,
+    this.subtitleTextStyle,
+    this.switchActiveColor, this.title, this.subtitle, this.subtitleMaxLines, this.onToggle, this.switchValue,
+  })  : _tileType = _SettingsTileType.sliderTile,
+        onTap = null,
+        onPressed = null,
+        iosChevron = null,
+        iosChevronPadding = null,
+        assert(titleMaxLines == null || titleMaxLines > 0),
+        assert(value != null),
+        assert(min != null),
+        assert(max != null),
+        assert(value >= min && value <= max),
+        assert(divisions == null || divisions > 0),
+        assert(thumbColor != null),
         super(key: key);
 
   @override
@@ -83,7 +127,33 @@ class SettingsTile extends StatelessWidget {
   }
 
   Widget iosTile(BuildContext context) {
-    if (_tileType == _SettingsTileType.switchTile) {
+    if (_tileType == _SettingsTileType.sliderTile) {
+      return CupertinoSettingsItem(
+        enabled: enabled,
+        type: SettingsItemType.slider,
+        label: title,
+        labelMaxLines: titleMaxLines,
+        leading: leading,
+        subtitle: subtitle,
+        subtitleMaxLines: subtitleMaxLines,
+        switchValue: switchValue,
+        onToggle: onToggle,
+        labelTextStyle: titleTextStyle,
+        switchActiveColor: switchActiveColor,
+        subtitleTextStyle: subtitleTextStyle,
+        valueTextStyle: subtitleTextStyle,
+        trailing: trailing,
+        sliderValue: value,
+        max: max,
+        min: min,
+        thumbColor: thumbColor,
+        activeColor: activeColor,
+        onChangeStart: onChangeStart,
+        onChangeEnd: onChangeEnd,
+        onChanged: onChanged,
+        divisions: divisions,
+      );
+    } else if (_tileType == _SettingsTileType.switchTile) {
       return CupertinoSettingsItem(
         enabled: enabled,
         type: SettingsItemType.toggle,
@@ -141,6 +211,28 @@ class SettingsTile extends StatelessWidget {
                 overflow: TextOverflow.ellipsis,
               )
             : null,
+      );
+    } else if (_tileType == _SettingsTileType.sliderTile) {
+      return ListTile(
+        title: Slider(
+          value: value,
+          min: min,
+          max: max,
+          onChangeStart: onChangeStart,
+          onChangeEnd: onChangeEnd,
+          onChanged: onChanged,),
+        subtitle: subtitle != null
+            ? Text(
+          subtitle,
+          style: subtitleTextStyle,
+          maxLines: subtitleMaxLines,
+          overflow: TextOverflow.ellipsis,
+        )
+            : null,
+        leading: leading,
+        enabled: enabled,
+        trailing: trailing,
+        onTap: onTapFunction(context),
       );
     } else {
       return ListTile(
