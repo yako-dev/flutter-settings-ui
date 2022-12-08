9856ff3e63cd263b83ebd1fb47b9cc8b153b6e8e
sawel24 <sawel.gnidenko@gmail.com>
2022-12-08 21:43:16 +0200

    feat: add integration tests for settings ui

--- example/lib/screens/gallery/ios_developer_screen.dart
+++ example/lib/screens/gallery/ios_developer_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:settings_ui/settings_ui.dart';
 
 class IosDeveloperScreen extends StatefulWidget {
